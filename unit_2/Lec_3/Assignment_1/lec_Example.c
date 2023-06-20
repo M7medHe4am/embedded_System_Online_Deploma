3b5ae0772fe49f00629a9642b14e9e8a71c679ed
EtCh <m7medhe4am@gmail.com>
2023-06-20 16:19:02 +0300

    This is Lec_3 example solution

---
+++ unit_2/Lec_3/Assignment_1/lec_Example.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 19, 2023
+ *      Author: m7med
+ */
+
+#include<stdio.h>
+int main ()
+{
+	int i=0 ,j;
+
+	for(i=0;i<10;i++)
+	{
+
+		for(j=i;j<10;j++)
+		{
+			printf ("%d ",j);
+
+		}
+		printf("\n");
+
+	}
+
+	return 0;
+
+}
+
+
