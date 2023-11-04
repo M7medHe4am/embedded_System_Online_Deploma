662c34aadf2f5a94ac88be3c321aa3e4b67d07e7
EtCh <m7medhe4am@gmail.com>
2023-11-05 00:55:56 +0200

    linkedList new code

--- unit_4/Lesson_1/Assignment/linkedList_studentData/main.c
+++ unit_4/Lesson_1/Assignment/linkedList_studentData/main.c
@@ -1,89 +1,126 @@
 /*
  * main.c
  *
- *  Created on: Oct 21, 2023
+ *  Created on: Nov 4, 2023
  *      Author: m7med
  */
 
-#include "lL_buf.h"
+
+#include "linkedlist.h"
 
 
 int main()
 {
-	char temp[5];
-	int count;
+	char temp[20];
+
 	while(1)
 	{
-		DPRINTF("====================================\n");
+		DPRINTF("======================================\n");
 		DPRINTF("choose one of the following options.\n");
-		DPRINTF("====================================\n");
-		DPRINTF("Option 1 : ADD student.\n");
-		DPRINTF("Option 2 : Delete student.\n");
-		DPRINTF("Option 3 : View students.\n");
-		DPRINTF("Option 4 : Delete All students.\n");
-		DPRINTF("Option 5 : Display Specific student.\n");
-		DPRINTF("Option 6 : Display No. of students.\n");
-		DPRINTF("Option 7 : Display Specific student(in reverse order.)\n");
-		DPRINTF("Option 8 : Display the middle student.\n");
-		DPRINTF("Option 9 : Reverse the list.\n");
-		DPRINTF("====================================\n");
-		DPRINTF("Your option is: ");
+		DPRINTF("======================================\n");
+		DPRINTF("option 1 : Add student.\n");
+		DPRINTF("option 2 : Delete student.\n");
+		DPRINTF("option 3 : View students.\n");
+		DPRINTF("option 4 : Delete ALL student.\n");
+		DPRINTF("option 5 : get Nth student.\n");
+		DPRINTF("option 6 : get No of students.\n");
+		DPRINTF("option 7 : get middle student.\n");
+		DPRINTF("======================================\n");
+		DPRINTF("your option is: ");
 		gets(temp);
-		switch(atoi(temp))
+		switch (atoi(temp))
 		{
 		case 1:
 		{
-			add_student();
-			break;
+			LL_addStudent();
 		}
-
+		break;
 		case 2:
 		{
-			delete_student();
-			break;
+			unsigned int ID;
+			LL_status status;
+			DPRINTF("Enter student's ID:");
+			gets(temp);
+			ID=atoi(temp);
+			status=LL_deleteStudent(&ID);
+			if(status==NO_DATA)
+			{DPRINTF("No Data yet!!!\n");}
+			else if(status==ID_notFound)
+			{DPRINTF("ID doesn't Exist!!!\n");}
+			else
+			{DPRINTF("Deletion Succeeded\n");}
 		}
+		break;
 		case 3:
 		{
-			view_students();
-			break;
+			LL_status status;
+			status=LL_viewAll();
+			if(status==NO_DATA)
+				DPRINTF("No Data yet!!!\n");
 		}
+		break;
 		case 4:
 		{
-			deleteAll_students();
-			break;
+			LL_status status;
+			status=LL_deleteAll();
+			if(status==NO_DATA)
+			{DPRINTF("No Data yet!!!\n");}
+			else
+				DPRINTF("Deletion Succeeded\n");
 		}
-		case 5:
+		break;
+		case 5 :
 		{
-			get_studentid();
+			LL_status status;
+			unsigned int index;
+			DPRINTF("Enter index:");
+			index = atoi(gets(temp));
+			DPRINTF("to get Nth student enter : 1 \n");
+			DPRINTF("to get Nth student in reverse  enter : 2 \n");
+			DPRINTF("Your option is:");
+			switch (atoi(gets(temp)))
+			{
+			case 1 :
+			{status=getNth(&index);}
 			break;
+			case 2 :
+			{status=getNthReversed(&index);}
+			break;
+			default :
+			{DPRINTF("WRONG OPTION!!!\n");}
+			break;
+			}
+
+			if(status==NO_DATA)
+			{DPRINTF("No Data yet!!!\n");}
+			else if(status==ID_notFound)
+			{DPRINTF("ID doesn't Exist!!!\n");}
 		}
-		case 6:
+		break;
+		case 6 :
 		{
-			no_ofStudents(&count);
-			break;
+			int count=0;
+			if(countElements(&count)==NO_DATA)
+			{DPRINTF("No Data yet!!!\n");}
+			else
+			{DPRINTF("count is: %d\n",count);}
 		}
-		case 7:
+		break;
+		case 7 :
 		{
-			get_reverseIndex();
-			break;
+			if(getMiddleElement()==NO_DATA)
+			{DPRINTF("No Data yet!!!\n");}
 		}
-		case 8:
-				{
-					findMiddle();
-					break;
-				}
-		case 9:
-		{
-			reverse();
-			break;
-		}
+		break;
 		default :
 		{
-			DPRINTF("Wrong option!!!\n");
-			break;
+			DPRINTF("WRONG OPTION!!!\n");
 		}
 
 		}
+
 	}
 
+	return 0;
 }
+
