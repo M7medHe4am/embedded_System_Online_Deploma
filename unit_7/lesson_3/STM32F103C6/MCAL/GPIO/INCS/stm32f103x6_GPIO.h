fa98b22b54d0eaad600a4d5f47c0da0167c7375d
EtCh <m7medhe4am@gmail.com>
2024-08-26 23:43:03 +0300

    Perf : Updated The GPIO DRIVER

---
+++ unit_7/lesson_3/STM32F103C6/MCAL/GPIO/INCS/stm32f103x6_GPIO.h
@@ -0,0 +1,118 @@
+/*
+ * STM32F103Cx.h
+ *
+ *  Created on: Aug 26, 2024
+ *      Author: m7med
+ */
+
+#ifndef GPIO_INCS_STM32F103X6_GPIO_H_
+#define GPIO_INCS_STM32F103X6_GPIO_H_
+
+
+/*---------------------Includes---------------------*/
+
+#include "../../INCS/stm32f103x6.h"
+
+/*--------------------------------------------------*/
+
+/*---------------------GPIOConfigs---------------------*/
+typedef struct
+{
+
+	volatile uint8_t GPIO_PinNumber ;		// specifies the GPIO pins to be configured
+	                                        //This parameter must be a value of @ref GPIO_PINS_define
+
+	volatile uint8_t GPIO_Mode ;			// specifies the GPIO MODE to be configured
+											//This parameter must be a value of @ref GPIO_MODE_define`
+
+	volatile uint8_t GPIO_Speed ;			//specifies the speed of the selected pins
+											//This parameter must be set based on @ref GPIO_SPEED_define
+
+
+}GPIO_PinConfig_t;
+
+
+
+
+
+//@ref GPIO_PINS_define
+#define GPIO_PIN_0	  0
+#define GPIO_PIN_1	  4
+#define GPIO_PIN_2	  8
+#define GPIO_PIN_3	  12
+#define GPIO_PIN_4	  16
+#define GPIO_PIN_5	  20
+#define GPIO_PIN_6	  24
+#define GPIO_PIN_7	  28
+#define GPIO_PIN_8	  32
+#define GPIO_PIN_9	  36
+#define GPIO_PIN_10	  40
+#define GPIO_PIN_11	  44
+#define GPIO_PIN_12	  48
+#define GPIO_PIN_13	  52
+#define GPIO_PIN_14	  56
+#define GPIO_PIN_15	  60
+
+
+
+
+
+
+
+
+//@ref GPIO_MODE_define
+/*In input mode (MODE[1:0]=00):
+0: Analog mode
+1: Floating input (reset state)
+2: Input with pull-up
+3: Input with pull-down
+In output mode (MODE[1:0] > 00):
+4: General purpose output push-pull
+5: General purpose output Open-drain
+6: Alternate function output Push-pull
+7: Alternate function output Open-drain
+8: Alternate function input
+*/
+//analoge mode
+#define GPIO_MODE_IN_Analog 0b0000
+//input modes
+#define GPIO_MODE_IN_FLO  	0b0100
+#define GPIO_MODE_IN_PU   	0b1000
+#define GPIO_MODE_IN_PD	  	0b1000
+//output modes
+#define GPIO_MODE_OUT_PP    0b0000
+#define GPIO_MODE_OUT_OD    0b0100
+#define GPIO_MODE_OUT_AF_PP	0b1000
+#define GPIO_MODE_OUT_AF_OD	0b1100
+
+//@ref GPIO_SPEED_define
+//0: Input mode (reset state)
+//1: Output mode, max speed 10 MHz.
+//2: Output mode, max speed 2 MHz.
+//3: Output mode, max speed 50 MHz.
+
+#define GPIO_SPEED_10M		0b01
+#define GPIO_SPEED_2M		0b10
+#define GPIO_SPEED_50M		0b11
+
+
+
+
+
+
+
+
+
+
+//* ===============================================
+//* APIs Supported by "MCAL GPIO DRIVER"
+//* ===============================================
+void MCAL_GPIO_init(GPIO_TypeDef_t* GPIOx ,GPIO_PinConfig_t* PinConfig);
+uint8_t MCAL_GPIO_ReadPIN(GPIO_TypeDef_t* GPIOx , uint8_t PinNumber);
+uint16_t MCAL_GPIO_ReadPORT(GPIO_TypeDef_t* GPIOx);
+void MCAL_GPIO_WritePIN(GPIO_TypeDef_t* GPIOx ,uint8_t PinNumber , uint8_t value);
+void MCAL_GPIO_WritePORT(GPIO_TypeDef_t* GPIOx , uint16_t value);
+
+/*--------------------------------------------------*/
+
+#endif /* GPIO_INCS_STM32F103X6_GPIO_H_ */
---
+++ unit_7/lesson_3/STM32F103C6/MCAL/GPIO/SOURCE/stm32f103x6_GPIO.c
@@ -0,0 +1,137 @@
+
+
+/*---------------------Includes---------------------*/
+
+#include "../INCS/stm32f103x6_GPIO.h"
+/*--------------------------------------------------*/
+
+
+//* ===============================================
+//* APIs Supported by "MCAL GPIO DRIVER"
+//* ===============================================
+
+
+
+/**================================================================
+ * @Fn				-MCAL_GPIO_Init
+ * @brief 			-Initializes the GPIOx PINy according to the specified parameters in the PinConfig
+ * @param [in]		-GPIOx: where x can be (A...E depending on the device used) to select the GPIO peripheral
+ * @param [in] 		-PinConfig: Ptr to a GPIO_PinConfig_t structure that contains the configuration information for
+ * 								the specified GPIO pin.
+ * @retval 			-none
+ * Note				-stm32F103C6 MCU has GPIO A,B,C,D,E modules
+ * 					 But LQFP48 package has only GPIO A,B,PART of C,D exported as external pins from the MCU
+ */
+void MCAL_GPIO_init(GPIO_TypeDef_t* GPIOx ,GPIO_PinConfig_t* PinConfig)
+{
+	volatile uint32_t* CNFG_Reg = &GPIOx->CRL ;
+	uint8_t pinNumber = PinConfig->GPIO_PinNumber ;
+
+	//======MODE CONFIG======
+
+	if(pinNumber > GPIO_PIN_7)
+	{
+		CNFG_Reg = &GPIOx->CRH;
+		pinNumber -=32 ;
+
+	}
+
+	//Clearing the pin's 4 bits
+	*CNFG_Reg &= ~(0xF << pinNumber) ;
+
+	//SETTING MODE (IN/OUT)
+	if(PinConfig->GPIO_Mode == GPIO_MODE_IN_PU)
+	{
+		GPIOx->ODR |= 1 << pinNumber ;
+	}
+	else if(PinConfig->GPIO_Mode == GPIO_MODE_IN_PD)
+	{
+		GPIOx->ODR &= ~ (1 << pinNumber) ;
+	}
+
+	*CNFG_Reg |= PinConfig->GPIO_Mode << pinNumber ;
+
+	//Setting Speed if output
+
+	*CNFG_Reg |=PinConfig->GPIO_Speed << pinNumber ;
+
+
+}
+
+
+
+/**================================================================
+ * @Fn				-MCAL_GPIO_ReadPIN
+ * @brief 			-Read specific pin
+ * @param [in]		-GPIOx: where x can be (A...E depending on the device used) to select the GPIO peripheral
+ * @param [in]		-pinNumber: Set the pin number according to @ref GPIO_PINS_Define
+ * @retval 			-the input pin value(two possible values based on @ref GPIO_PIN_state)
+ * Note				-none
+ */
+uint8_t MCAL_GPIO_ReadPIN(GPIO_TypeDef_t* GPIOx , uint8_t PinNumber)
+{
+
+	PinNumber /= 4;
+
+	return ((GPIOx->IDR & (1<<PinNumber)) >> PinNumber) ;
+
+}
+
+/**================================================================
+ * @Fn				-MCAL_GPIO_ReadPORT
+ * @brief 			-Read specific PORT
+ * @param [in]		-GPIOx: where x can be (A...E depending on the device used) to select the GPIO peripheral
+ * @retval 			-the input port value
+ * Note				-none
+ */
+uint16_t MCAL_GPIO_ReadPORT(GPIO_TypeDef_t* GPIOx)
+{
+
+	return (uint16_t)GPIOx->ODR ;
+
+}
+
+/**================================================================
+ * @Fn				-MCAL_GPIO_WritePIN
+ * @brief 			-Writes on specific PIN
+ * @param [in]		-GPIOx: where x can be (A...E depending on the device used) to select the GPIO peripheral
+ * @param [in]		-pinNumber: Set the pin number according to @ref GPIO_PINS_Define
+ * @param [in]		-value: Pin Value
+ * @retval 			-none
+ * Note				-none
+ */
+
+void MCAL_GPIO_WritePIN(GPIO_TypeDef_t* GPIOx ,uint8_t PinNumber , uint8_t value)
+{
+
+	PinNumber /= 4;
+	if (value == 0 )
+	{
+		GPIOx->ODR &= ~(1<<PinNumber);
+	}
+	else
+	{
+		GPIOx->ODR |= 1<<PinNumber ;
+	}
+
+
+
+
+}
+
+/**================================================================
+ * @Fn				-MCAL_GPIO_WritePORT
+ * @brief 			-Writes on specific PORT
+ * @param [in]		-GPIOx: where x can be (A...E depending on the device used) to select the GPIO peripheral
+ * @param [in]		-value: PORT Value
+ * @retval 			-none
+ * Note				-none
+ */
+void MCAL_GPIO_WritePORT(GPIO_TypeDef_t* GPIOx , uint16_t value)
+{
+	GPIOx->ODR = (uint32_t)value ;
+}
+
+
+
+
